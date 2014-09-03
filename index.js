248e1a43792d2cbb13e5c19291d109d3d9016011
kirk7880@gmail.com <kirk7880@gmail.com>
2014-09-03 09:46:45 -0400

    Initial attempt to browserify

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
 /* jshint unused:false */
+(function() {
+  'use strict';
+  var root = this;
+  var previous = root.jsonPromise || null;
+  var jsonPromise = {};
 
-'use strict';
+  var isNode = (typeof require !== 'undefined' && typeof module !== 'undefined' && module.exports);
+  var JSONPromise = null;
 
-var JSONPromise = require('bluebird');
+  if (isNode) {
+    JSONPromise = require('bluebird');
+  } else {
+    JSONPromise = Promise;
+  }
 
-exports.parse = function(data) {
-  return new JSONPromise(function parse(resolve, reject) {
-    if (typeof data === (typeof {})) {
-      return resolve(data);
+  jsonPromise.parse = function(data) {
+    return new JSONPromise(function parse(resolve, reject) {
+      if (typeof data === (typeof {})) {
+        return resolve(data);
+      }
+
+      resolve(JSON.parse(data));
+    });
+  };
+
+  jsonPromise.stringify = function(data) {
+    return new JSONPromise(function stringify(resolve, reject) {
+      if (typeof data === (typeof '')) {
+        return resolve(data);
+      } 
+
+      resolve(JSON.stringify(data));
+    });
+  };
+
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = jsonPromise;
     }
 
-    resolve(JSON.parse(data));
-  });
-};
+    exports.jsonPromise = jsonPromise;
+  } else {
+    root.jsonPromise = jsonPromise;
+  }
 
-exports.stringify = function(data) {
-
-  return new JSONPromise(function stringify(resolve, reject) {
-    if (typeof data === (typeof '')) {
-      return resolve(data);
-    } 
-
-    resolve(JSON.stringify(data));
-  });
-};
+  jsonPromise.noConflict = function() {
+    root.jsonPromise = previous;
+    return jsonPromise;
+  };
+}).call(this);
